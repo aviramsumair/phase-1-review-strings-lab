8a835e75aa08f954c9af92eeff0a1e1713d73047
Avinash Ramsumair <avi@pure235games.com>
2022-11-20 22:09:20 -0500

    commit tree 5509f2909f063ae80e3c33ef99aeb348bed46679

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'Ollie Sun';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
